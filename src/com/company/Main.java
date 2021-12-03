8b874f852e3876ee8322292cf21e70721b610ca4
Ian Malloy <mellomalloy@gmail.com>
2021-12-03 09:51:21 +0800

    Today I changed how the background color of the user interface

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        /*
+         * Practice problems.  Problems can have multiple correct implementations,
+         * and can be solved in multiple ways.
+         * functions can be used on all problems, if so desired.
+         *
+         *
+         */
+
+    }
+
+    //print all numbers between 1 - 100
+
+    //print only even numbers between 1 - 100
+
+    //print all odd numbers between 1 - 100
+
+    /*
+     * Create functions named "A", "B", "C", "D", and "E"
+     * "A","B","C","D" should take in two Integer variables(parameters) and return an integer
+     * "A" should return the sum of the two parameters
+     * "B" should return the difference of the two parameters
+     * "C" should return the product of the two parameters
+     * "D" should return the quotient of the two parameters
+     * "E" should have one parameter, and not return anything
+     *
+     */
+}
+
